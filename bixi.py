c6993b016beb19c5a1b4dc82ef63c13c423a3ecd
Olivier Thereaux <ot@zoy.org>
2011-12-13 08:49:11 +0100

    location of the map files changed, again

--- bixi.py
+++ bixi.py
@@ -12,7 +12,7 @@ USE AT YOUR OWN RISKS
 import sys
 import os
 import xml.etree.cElementTree as ET
-from urllib import urlopen
+from urllib2 import urlopen
 import re
 
 def main():
@@ -20,8 +20,9 @@ def main():
     <kml xmlns="http://earth.google.com/kml/2.1">
     <Document><name>Bixi</name><Style id="BixiIconStyle"><scale>1,.5</scale><IconStyle><Icon><href>http://olivier.thereaux.net/2009/05/bixi_icon.png</href></Icon></IconStyle></Style>
     """
-    for URL in ["https://profil.bixi.ca/data/bikeStations.xml", "https://toronto.bixi.com/data/bikeStations.xml"]:
-        tree = ET.parse(urlopen(URL))
+    for URL in ["https://toronto.bixi.com/data/bikeStations.xml", "https://montreal.bixi.com/data/bikeStations.xml"]:
+        feed = urlopen(URL)
+        tree = ET.parse(feed)
         for station in tree.getroot().findall("station"):
             nbBikes = station.find("nbBikes").text
             nbEmptyDocks = station.find("nbEmptyDocks").text
@@ -31,6 +32,7 @@ def main():
             station_locked = station.find("locked").text
             if (station_installed == "true" and station_locked != "true" and (nbBikes != "0" or nbEmptyDocks != "0")):
                 print "<Placemark><name>%s bikes avail. - %s empty slots</name><styleUrl>#BixiIconStyle</styleUrl><Point><coordinates>%s,%s,0</coordinates></Point></Placemark>" % (nbBikes, nbEmptyDocks, station_long, station_lat)
+        feed.close()
     print "</Document></kml>"
     
 if __name__ == '__main__':
